b6cd3747fad8170b8ed73772e6b22945d5312f0c
hakehash <b7dedc8797ec8ty@gmail.com>
2024-04-21 16:40:03 +0900

    cd

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,15 @@
+function cd(){
+  Param($PATH=$HOME)
+  if ($PATH -eq '-') {
+    $TEMPPWD = Get-Location
+    Set-Location $OLDPWD
+    $global:OLDPWD = $TEMPPWD
+    (Get-Location).Path
+  } else {
+    $global:OLDPWD = Get-Location
+    Set-Location $PATH
+  }
+}
 function touch() {
   Param($FILE)
   if (Test-Path $FILE) {
@@ -9,7 +21,13 @@ function touch() {
 function iexplore() {
   (New-Object -ComObject InternetExplorer.Application).Visible = $True
 }
+function ls(){
+  Param($PATH)
+  Get-ChildItem $PATH -Name
+}
 Remove-Item Alias:curl
+Remove-Item Alias:cd
+Remove-Item Alias:ls
 Set-Alias vi vim
 Set-PSReadLineOption -EditMode Emacs
 Set-PSReadLineKeyHandler -Chord Ctrl+y -Function Paste
